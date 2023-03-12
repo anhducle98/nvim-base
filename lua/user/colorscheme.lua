598044e92cd15ceeeb1d13fd2da9f6df39f18d8d
Anh-Duc Le <anhducle98@gmail.com>
2023-03-12 15:10:09 +0800

    Update

--- lua/user/colorscheme.lua
+++ lua/user/colorscheme.lua
@@ -2,23 +2,31 @@
 -- local colorscheme = "catppuccin"
 local colorscheme = "kanagawa"
 
+
+-- Default options:
 require('kanagawa').setup({
-  undercurl = false,
-  commentStyle = { italic = true },
-  functionStyle = {},
-  keywordStyle = { italic = false },
-  statementStyle = { bold = true },
-  typeStyle = { italic = false },
-  variablebuiltinStyle = { italic = false },
-  specialReturn = true,       -- special highlight for the return keyword
-  specialException = true,    -- special highlight for exception handling keywords
-  transparent = false,        -- do not set background color
-  dimInactive = false,        -- dim inactive window `:h hl-NormalNC`
-  globalStatus = false,       -- adjust window separators highlight for laststatus=3
-  terminalColors = true,      -- define vim.g.terminal_color_{0,17}
-  colors = {},
-  overrides = {},
-  theme = "default"           -- Load "default" theme or the experimental "light" theme
+    compile = false,             -- enable compiling the colorscheme
+    undercurl = false,            -- enable undercurls
+    commentStyle = { italic = false },
+    functionStyle = {},
+    keywordStyle = { italic = false },
+    statementStyle = { bold = true },
+    typeStyle = { italic = false },
+    transparent = false,         -- do not set background color
+    dimInactive = false,         -- dim inactive window `:h hl-NormalNC`
+    terminalColors = true,       -- define vim.g.terminal_color_{0,17}
+    colors = {                   -- add/modify theme and palette colors
+        palette = {},
+        theme = { wave = {}, lotus = {}, dragon = {}, all = {} },
+    },
+    overrides = function(colors) -- add/modify highlights
+        return {}
+    end,
+    theme = "wave",
+    background = {
+        dark = "wave",
+        light = "lotus"
+    },
 })
 
 local status_ok, _ = pcall(vim.cmd.colorscheme, colorscheme)
