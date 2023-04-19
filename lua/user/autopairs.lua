b25eaabcd6bb35687e075aabc94647e807f4f92a
Anh Duc Le <ale@dytechlab.com>
2023-04-19 21:37:42 +0800

    Update autopairs config

--- lua/user/autopairs.lua
+++ lua/user/autopairs.lua
@@ -24,6 +24,8 @@ npairs.setup {
     highlight = "PmenuSel",
     highlight_grey = "LineNr",
   },
+  ignored_next_char = "[%w%.]", -- will ignore alphanumeric and `.` symbol
+  enable_check_bracket_line = false,
 }
 
 local cmp_autopairs = require "nvim-autopairs.completion.cmp"
